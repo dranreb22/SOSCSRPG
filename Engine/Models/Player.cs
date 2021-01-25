a8b4f10816207d81b5a500dfa3bec012d3d6bec1
berna <bernard2200@hotmail.com>
2021-01-25 12:41:31 -0500

    enabling listener on player class to update view when variables update in controller/view model

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,16 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Engine.Models
 {
-    public class Player
+    public class Player : INotifyPropertyChanged
     {
-        public string CharacterClass { get; set; }
-        public int HitPoints { get; set; }
+        private int _hitPoints;
+        private string _class;
+        private int _level;
+        private int _experiencePoints;
+        private int _gold;
+
+        public string CharacterClass
+        {
+            get { return _class; }
+            set
+            {
+                _class = value;
+                OnPropertyChanged("CharacterClass");
+            }
+        }
+        public int HitPoints {
+            get { return _hitPoints; }
+            set
+            {
+                _hitPoints = value;
+                OnPropertyChanged("HitPoints");
+            }
+        }
         public string Name { get; set; }
-        public int ExperiencePoints { get; set; }
-        public int Level { get; set; }
-        public int Gold { get; set; }
+        public int ExperiencePoints { 
+            get { return _experiencePoints; }
+            set 
+            { 
+                _experiencePoints = value;
+                OnPropertyChanged("ExperiencePoints");
+            }
+        }
+        public int Level {
+            get { return _level; }
+            set {
+                _level = value;
+                OnPropertyChanged("Level");
+            } 
+        }
+        public int Gold {
+            get { return _gold; }
+            set
+            {
+                _gold = value;
+                OnPropertyChanged("Gold");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
