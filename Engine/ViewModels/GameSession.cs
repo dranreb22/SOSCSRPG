76276a123f60cf05c4c2c95e5b584d71342d62b6
berna <bernard2200@hotmail.com>
2021-01-25 21:52:41 -0500

    added location class and files. however, will be converting to framework instead of core on next commit

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -8,6 +8,8 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+
+        public Location CurrentLocation { get; set; }
         
         public GameSession()
         {
@@ -19,6 +21,13 @@ namespace Engine.ViewModels
             CurrentPlayer.HitPoints = 10;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your house.";
+            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
         }
     }
 }
